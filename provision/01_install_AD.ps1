16fd7b4b87759917ffc3a73d5b13f59c5af85c10
Dominique Broeglin <dominique.broeglin@gmail.com>
2015-11-03 22:04:12 +0100

    First shot at provisioning a dc and a client

---
+++ provision/01_install_AD.ps1
@@ -0,0 +1,7 @@
+Add-WindowsFeature "RSAT-AD-Tools"
+
+start-job -Name addFeature -ScriptBlock { 
+Add-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "gpmc" -IncludeAllSubFeature -IncludeManagementTools } 
+Wait-Job -Name addFeature
---
+++ provision/02_install_forest.ps1
@@ -0,0 +1,16 @@
+$domainname = "broeglin.fr"
+$netbiosName = "BROEGLIN" 
+
+Import-Module ADDSDeployment 
+Install-ADDSForest -CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2012" `
+-DomainName $domainname `
+-DomainNetbiosName $netbiosName `
+-ForestMode "Win2012" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-safemodeadministratorpassword (convertto-securestring "Password1" -asplaintext -force) `
+-Force:$true
