264159fb5f8329284505d55e100844cad9b7d6f1
Dominique Broeglin <dominique.broeglin@gmail.com>
2016-04-30 16:39:19 +0200

    Typo in identityType, force all servers to use DC as DNS

--- provision/01_install_AD.ps1
+++ provision/01_install_AD.ps1
@@ -3,6 +3,10 @@ $ErrorActionPreference = "Stop"
 $IPAddress  = "172.16.124.50"
 $VagrantIPPattern = "10.0.*"
 
+#
+# Ensure AD and DNS use only the $IPAddress 
+#
+
 Get-NetIPConfiguration -InterfaceIndex (Get-NetIPAddress -IPAddress $VagrantIPPattern).InterfaceIndex |
     Get-NetConnectionProfile |
     Where IPv4Connectivity -ne "NoTraffic" |
@@ -11,12 +15,17 @@ Get-NetIPConfiguration -InterfaceIndex (Get-NetIPAddress -IPAddress $VagrantIPPa
 Get-NetAdapter -InterfaceIndex (Get-NetIPAddress -IPAddress $IPAddress).InterfaceIndex | 
     Rename-NetAdapter -NewName Lab
 
+
+#
+# Install AD features
+#
+
 Add-WindowsFeature "RSAT-AD-Tools"
 
 Add-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools 
 Add-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools 
 Add-WindowsFeature -Name "gpmc" -IncludeAllSubFeature -IncludeManagementTools 
 
-# Force the DNS server on one of the IPs
+# Force the DNS server to bind to $IPAddress
 dnscmd dc01 /ResetListenAddresses $IPAddress
 
